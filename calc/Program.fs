54f7af82fc20a8316380fbfbe304e0ec1e380bb2
Seyeon Jeong <best6653@gmail.com>
2023-09-01 09:26:32 +0900

    init commit 2

---
+++ calc/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open FSharp.Text.Lexing
+
+let evaluate (input:string) =
+    let lexbuf = LexBuffer<char>.FromString input
+    let output = Parser.input Lexer.token lexbuf
+    string output
+    
+
+let calc () = 
+    printfn "Press Ctrl+c to Exit"
+
+    while true do
+        printf "Evaluate > "
+        let input = Console.ReadLine()
+        try
+            let result = evaluate input
+            printfn "%s" result
+        with ex -> printfn "%s" (ex.ToString())
+   
+
+[<EntryPoint>]
+let main argv =
+    calc ()
+    0
