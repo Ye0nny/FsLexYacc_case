8a957823bb67ad68b7c6cf195eed4832cdedbf2d
Seyeon Jeong <best6653@gmail.com>
2023-09-04 15:10:21 +0900

    WIP) add AST

---
+++ calc/Ast.fs
@@ -0,0 +1,10 @@
+﻿module Ast
+
+type Expr =
+    | Num of float
+    | Plus of Expr * Expr
+    | Minus of Expr * Expr
+    | Mul of Expr * Expr
+    | Div of Expr * Expr
+    | Mod of Expr * Expr
+
