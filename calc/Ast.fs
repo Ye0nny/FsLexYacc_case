10db60dee1cbc77a3ba8e82c28bf33413ce588ce
Seyeon Jeong <best6653@gmail.com>
2023-09-04 16:10:22 +0900

    add evaluate() for expr in AST

--- calc/Ast.fs
+++ calc/Ast.fs
@@ -7,3 +7,24 @@ type Expr =
     | Mul of Expr * Expr
     | Div of Expr * Expr
 
+
+let rec _evaluate (p1:Expr) =
+    let result =
+        match p1 with
+        | Num(x) -> x
+        | Plus(Num(x), Num(y)) -> x + y
+        | Minus(Num(x), Num(y)) -> x - y
+        | Mul(Num(x), Num(y)) -> x * y
+        | Div(Num(x), Num(y)) -> x / y
+        | Plus(x, y) -> (_evaluate x) + (_evaluate y)
+        | Minus(x, y) -> (_evaluate x) - (_evaluate y)
+        | Mul(x, y) -> (_evaluate x) * (_evaluate y)
+        | Div(x, y) -> (_evaluate x) / (_evaluate y)
+
+    result
+
+let eval (p1:Expr) =
+    printfn "%.10A" p1
+
+    let result = _evaluate (p1)
+    printfn "%.10A" result
